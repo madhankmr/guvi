7333ea81558281b44734a1e281d45f1f232af8f4
madhankmr <51670567+madhankmr@users.noreply.github.com>
2019-06-24 00:02:42 -0700

    Create assignment2.py

---
+++ assignment2.py
@@ -0,0 +1,18 @@
+A=2
+B=3
+C=4
+D=5
+E=6
+F=7
+G=8
+H=1
+I=9
+J=10
+add=A+B
+sub=A-B
+mul=A*B
+div=A/B
+print("sum of A and B is :" ,add)
+print("sub of J and C is :" ,sub)
+print("mul of I and G is :" ,mul)
+print("div of G and A is :" ,div
